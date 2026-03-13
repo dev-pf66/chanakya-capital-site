f6e81837193d643c5e51542736d233e35ac7d365
dev-pf66 <dev@pocket-fund.com>
2026-03-13 17:59:40 +0530

    Add Google Analytics tag G-DRKJGZEVT1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
@@ -74,6 +75,19 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen flex flex-col font-sans">
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DRKJGZEVT1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DRKJGZEVT1');
+          `}
+        </Script>
         <Nav />
         <main className="flex-1">
           {children}
